cc3f63e404ae3f56e2a19d1dd872b7c1f98f181d
csyer <2960069894@qq.com>
2024-01-04 10:51:08 +0800

    upd: lsb: 写累了，摆一会

--- riscv/src/alu.v
+++ riscv/src/alu.v
@@ -1,20 +1,21 @@
 `include "def.v"
 
-`define FUNCT3_ADD  3'h0
-`define FUNCT3_XOR  3'h4
-`define FUNCT3_OR   3'h6
-`define FUNCT3_AND  3'h7
-`define FUNCT3_SLL  3'h1
-`define FUNCT3_SRL  3'h5
-`define FUNCT3_SRA  3'h5
-`define FUNCT3_SLT  3'h2
-`define FUNCT3_SLTU 3'h3
-`define FUNCT3_BEQ  3'h0
-`define FUNCT3_BNE  3'h1
-`define FUNCT3_BLT  3'h4
-`define FUNCT3_BGE  3'h5
-`define FUNCT3_BLTU 3'h6
-`define FUNCT3_BGEU 3'h7
+`define FUNCT3_ADD  3'b000
+`define FUNCT3_SLL  3'b001
+`define FUNCT3_SLT  3'b010
+`define FUNCT3_SLTU 3'b011
+`define FUNCT3_XOR  3'b100
+`define FUNCT3_SRL  3'b101
+`define FUNCT3_SRA  3'b101
+`define FUNCT3_OR   3'b110
+`define FUNCT3_AND  3'b111
+
+`define FUNCT3_BEQ  3'b000
+`define FUNCT3_BNE  3'b001
+`define FUNCT3_BLT  3'b100
+`define FUNCT3_BGE  3'b101
+`define FUNCT3_BLTU 3'b110
+`define FUNCT3_BGEU 3'b111
 
 module ALU(
     input wire clk,

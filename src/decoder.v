66c61347fdb39830cd3a6442ae1cfb8e1551017d
csyer <2960069894@qq.com>
2024-01-04 16:20:02 +0800

    upd: decoder: 从总线更新；fix: 调整了文件结构

---
+++ src/decoder.v
@@ -0,0 +1,192 @@
+`ifndef DECODER
+`define DECODER
+
+`include "def.v"
+
+module Decoder(
+    input wire clk,
+    input wire rst,
+    input wire rdy,
+
+    input wire rollback,
+
+    // fetch from IFetch
+    input wire inst_done,
+    input wire [`INST_WID] inst,
+    input wire [`ADDR_WID] inst_pc,
+    input wire inst_pre_j,
+
+    // issue
+    output reg issue,
+    output reg [`ROB_WID] rob_pos,
+    output reg [6:0] opcode,
+    output reg [2:0] funct3,
+    output reg funct7,
+    output reg rs1_rdy,
+    output reg [`DATA_WID] rs1_val,
+    output reg [`ROB_WID] rs1_rob_pos,
+    output reg rs2_rdy,
+    output reg [`DATA_WID] rs2_val,
+    output reg [`ROB_WID] rs2_rob_pos,
+    output reg [`DATA_WID] imm,
+    output reg [`REG_WID] rd,
+    output reg [`ADDR_WID] pc,
+    output reg pre_j,
+
+    // query from RegFile
+    output wire [`REG_WID] reg_rs1,
+    input wire reg_rs1_rdy,
+    input wire [`DATA_WID] reg_rs1_val,
+    input wire [`ROB_WID] reg_rs1_rob_pos,
+    output wire [`REG_WID] reg_rs2,
+    input wire reg_rs2_rdy,
+    input wire [`DATA_WID] reg_rs2_val,
+    input wire [`ROB_WID] reg_rs2_rob_pos,
+
+    output reg rs_en,
+    output reg lsb_en,
+
+    input wire alu_done,
+    input wire [`DATA_WID] alu_res,
+    input wire [`ROB_WID] alu_res_rob_pos,
+
+    input wire lsb_done,
+    input wire [`DATA_WID] lsb_res,
+    input wire [`ROB_WID] lsb_res_rob_pos,
+
+    input wire [`ROB_WID] rob_rs1_pos,
+    output wire rob_rs1_rdy,
+    output wire rob_rs1_val,
+
+    input upd_rob_pos
+);
+
+assign reg_rs1 = inst[19:15];
+assign reg_rs2 = inst[24:20];
+
+assign rob_rs1_pos = reg_rs1_rob_pos;
+assign rob_rs2_pos = reg_rs2_rob_pos;
+
+always @(*) begin
+    rob_pos = upd_rob_pos;
+    opcode = inst[6:0];
+    funct3 = inst[14:12];
+    funct7 = inst[30];
+    rd = inst[11:7];
+    imm = 0;
+    pc = inst_pc;
+    pre_j = inst_pre_j;
+
+    is_ready = 0;
+
+    issue = 0;
+    rs1_rdy = 1;
+    rs1_val = 0;
+    rs1_rob_pos = 0;
+    rs2_rdy = 1;
+    rs2_val = 0;
+    rs2_rob_pos = 0;
+
+    if (rst || !inst_done || rollback) begin
+    end else if (rdy) begin
+        issue = 1;
+
+        // RS LSB RoB 都可能更新依赖
+        if (reg_rs1_rdy) begin
+            rs1_val = reg_rs1_val;
+        end else if (alu_done && reg_rs1_rob_pos == alu_res_rob_pos) begin
+            rs1_val = alu_res;
+        end else if (lsb_done && reg_rs1_rob_pos == lsb_res_rob_pos) begin
+            rs1_val = lsb_res;
+        end else if (rob_rs1_rdy) begin
+            rs1_val = rob_rs1_val;
+        end begin 
+            rs1_val = 0;
+            rs1_rdy = reg_rs1_rdy;
+            rs1_rob_pos = reg_rs1_rob_pos;
+        end
+
+        if (reg_rs2_rdy) begin
+            rs2_val = reg_rs2_val;
+        end else if (alu_done && reg_rs2_rob_pos == alu_res_rob_pos) begin
+            rs2_val = alu_res;
+        end else if (lsb_done && reg_rs2_rob_pos == lsb_res_rob_pos) begin
+            rs2_val = lsb_res;
+        end else if (rob_rs2_rdy) begin
+            rs2_val = rob_rs2_val;
+        end begin 
+            rs2_val = 0;
+            rs2_rdy = reg_rs2_rdy;
+            rs2_rob_pos = reg_rs2_rob_pos;
+        end
+
+        case (opcode)
+            `OPCODE_L: begin
+                lsb_en = 1;
+                rs2_rdy = 1;
+                rs2_val = 0;
+                rs2_rob_pos = 0;
+                imm = {{21{inst[31]}}, inst[30:20]};
+            end
+            `OPCODE_S: begin
+                lsb_en = 1;
+                rd = 0;
+                imm = {{21{inst[31]}}, inst[30:20]};
+            end
+            `OPCODE_CAL: begin
+                rs_en = 1;
+            end
+            `OPCODE_CALI: begin
+                rs_en = 1;
+                rs2_rdy = 1;
+                rs2_val = 0;
+                rs2_rob_pos = 0;
+                imm = {{21{inst[31]}}, inst[30:20]};
+            end
+            `OPCODE_B: begin
+                rs_en = 1;
+                rd = 0;
+                imm = {{20{inst[31]}}, inst[7], inst[30:25], inst[11:8], 1'b0};
+            end
+            `OPCODE_LUI: begin
+                rs_en = 1;
+                rs1_rdy = 1;
+                rs1_val = 0;
+                rs1_rob_pos = 0;
+                rs2_rdy = 1;
+                rs2_val = 0;
+                rs2_rob_pos = 0;
+                imm = {inst[31:12], 12'b0};
+            end
+            `OPCODE_AUIPC: begin
+                rs_en = 1;
+                rs1_rdy = 1;
+                rs1_val = 0;
+                rs1_rob_pos = 0;
+                rs2_rdy = 1;
+                rs2_val = 0;
+                rs2_rob_pos = 0;
+                imm = {inst[31:12], 12'b0};
+            end
+            `OPCODE_JAL: begin
+                rs_en = 1;
+                rs1_rdy = 1;
+                rs1_val = 0;
+                rs1_rob_pos = 0;
+                rs2_rdy = 1;
+                rs2_val = 0;
+                rs2_rob_pos = 0;
+                imm = {{12{inst[31]}}, inst[19:12], inst[20], inst[30:21], 1'b0};
+            end
+            `OPCODE_JALR: begin
+                rs_en = 1;
+                rs2_rdy = 1;
+                rs2_val = 0;
+                rs2_rob_pos = 0;
+                imm = {{21{inst[31]}}, inst[30:20]};
+            end
+        endcase
+    end
+end
+
+endmodule
---
+++ src/memctrl.v
@@ -0,0 +1,142 @@
+`ifndef MEM_CTRL
+`define MEM_CTRL
+
+`include "def.v"
+
+module MemCtrl(
+    input wire clk,
+    input wire rst,
+    input wire rdy,
+
+    input wire rollback, 
+
+    input wire [7:0] mem_din,
+    output reg [7:0] mem_dout,
+    output reg [31:0] mem_a,
+    output reg mem_wr,
+
+    input wire if_en,
+    input wire [`ADDR_WID] if_pc,
+    output reg if_done,
+    output wire [`ICACHE_LINE_WID] if_data,
+
+    input wire lsb_en,
+    input wire lsb_wr,
+    input wire lsb_a,
+    input wire lsb_l,
+    input wire lsb_w,
+    output reg lsb_r,
+    output reg lsb_done,
+
+    input wire io_buffer_full
+);
+
+reg [1:0] status; //0: IDLE, 1: IF, 2: LOAD, 3: STORE
+reg [6:0] stage; // 2^6 = 64
+reg [6:0] len;
+
+reg [7:0] _if_data[`ICACHE_LINE_SIZ - 1:0];
+
+reg [`ADDR_WID] store_a;
+
+genvar i;
+generate
+    for (i = 0; i < `ICACHE_LINE_SIZ; i = i + 1) begin
+        assign if_data[i * 8 + 7:i * 8] = _if_data[i];
+    end
+endgenerate
+
+always @(posedge clk) begin
+    if (rst) begin
+        status <= 0;
+        if_done <= 0;
+        lsb_done <= 0;
+        mem_a <= 0;
+        mem_wr <= 0;
+    end else if (rdy) begin
+        case(status)
+            mem_wr <= 0;
+            0: begin // IDLE
+                if (if_done || lsb_done) begin
+                    if_done <= 0;
+                    lsb_done <= 0;
+                end else if (!rollback) begin
+                    if (lsb_en) begin
+                        if (lsb_wr) begin // write / Store
+                            status <= 3; 
+                            store_a <= lsb_a;
+                        end else begin // read / Load
+                            status <= 2;
+                            mem_a <= lsb_a;
+                            lsb_r <= 0;
+                        end
+                    end else if (if_en) begin
+                        status <= 1;
+                        mem_a <= if_pc;
+                        stage <= 0;
+                        len <= `ICACHE_LINE_SIZ;
+                    end
+                end
+            end
+            1: begin // IF
+                _if_data[stage - 1] <= mem_din;
+                // mem 会迟一个周期把值送过来
+                if (stage + 1 == len) mem_a <= 0;
+                else mem_a <= mem_a + 1;
+                if (stage == len) begin
+                    if_done <= 1;
+                    mem_wr <= 0;
+                    mem_a <= 0;
+                    stage <= 0;
+                    status <= 0;
+                end else stage <= stage + 1;
+            end
+            2: begin // LOAD
+                if (rollback) begin
+                    status <= 0;
+                    lsb_done <= 0;
+                    mem_wr <= 0;
+                    mem_a <= 0;
+                    stage <= 0;
+                end else begin
+                    // stage 从 1 开始，迟一个周期
+                    if (stage != 0) begin
+                        lsb_r[stage * 8 - 1:(stage - 1) * 8] <= mem_din;
+                    end
+                    if (stage + 1 == len) mem_a <= 0;
+                    else mem_a = mem_a + 1;
+                    if (stage == len) begin
+                        status <= 0;
+                        lsb_done <= 1;
+                        mem_wr <= 0;
+                        mem_a <= 0;
+                        stage <= 0;
+                    end else stage <= stage + 1;
+                end
+            end
+            3: begin // STORE
+                if (!(store_a[17:16] == 2'b11 && io_buffer_full)) begin
+                    mem_wr <= 1;
+                    mem_dout <= lsb_w[stage * 8 + 7:stage * 8];
+                    if (stage == 0) mem_a <= store_a;
+                    else mem_a = mem_a + 1;
+                    if (stage == len) begin
+                        status <= 0;
+                        lsb_done <= 0;
+                        mem_a <= 0;
+                        stage <= 0;
+                    end else stage = stage + 1;
+                end
+            end
+        endcase
+    end else begin
+        if_done <= 0;
+        lsb_done <= 0;
+        mem_a <= 0;
+        mem_wr <= 0;
+    end
+end
+
+endmodule
+
+`endif
